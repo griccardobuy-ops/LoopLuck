872e8b65828629d855d22c4aab0e7a14cc5e7909
griccardobuy-ops <g.riccardobuy@gmail.com>
2026-07-11 13:26:09 +0200

    Initialize OneSignal service worker in SDK
    Added initialization script for OneSignal service worker.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,15 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+<script>
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+OneSignalDeferred.push(async function(OneSignal) {
+  await OneSignal.init({
+    appId: "0cfd448c-97c6-4772-b492-2a4238aeb9d3",
+    serviceWorkerPath: "LoopLuck/OneSignalSDKWorker.js",
+    serviceWorkerParam: {
+      scope: "/LoopLuck/"
+    }
+  });
+});
+</script>
